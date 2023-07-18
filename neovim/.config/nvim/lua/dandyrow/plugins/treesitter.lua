8d7c93d07dfa3d985682ebaf9514c397870d8c6a
dandyrow <development@daniellowry.co.uk>
2023-07-18 17:02:08 +0100

    Updated treesitter config
    Added treesitter rename capability and parenthases pair colouring.

--- neovim/.config/nvim/lua/dandyrow/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/dandyrow/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-refactor',
+    'HiPhish/nvim-ts-rainbow2',
+  },
   config = function ()
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
@@ -21,7 +25,32 @@ return {
 
       highlight = {
         enable = true,
-      }
+      },
+      -- Enable coloured bracket pairs
+      rainbow = {
+        enable = true,
+        query = 'rainbow-parens',
+        strategy = require('ts-rainbow').strategy.global,
+      },
+      -- Use treesitter for indentation
+      indent = {
+        enable = true,
+      },
+      refactor = {
+        highlight_definitions = {
+          enable = true,
+          clear_on_cursor_move = true,
+        },
+        highlight_current_scope = {
+          enable = true,
+        },
+        smart_rename = {
+          enable = true,
+          keymaps = {
+            smart_rename = '<leader>rn',
+          },
+        },
+      },
     }
   end
 }
