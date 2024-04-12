34b850aa767556294372c7dd24b4578445922966
Daniel Lowry <development@daniellowry.co.uk>
2024-04-12 17:18:07 +0100

    Enabled system clipboard use for WSL in neovim

--- neovim/.config/nvim/lua/dandyrow/options.lua
+++ neovim/.config/nvim/lua/dandyrow/options.lua
@@ -47,3 +47,15 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe'
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enable = 0,
+}
