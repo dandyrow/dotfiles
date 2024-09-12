099c9bc1ab22f82eda8b452ec4c82ce779fdf2b9
Daniel Lowry <development@daniellowry.co.uk>
2024-09-12 16:00:13 +0100

    ✨ Added systemd-boot nixos module
    Split out systemd-boot with some customised defaults.

---
+++ nix/nixosModules/systemd-boot.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, config, ... }: {
+  options = {
+    systemd-boot.enable = lib.mkEnableOption "enable systemd-boot";
+  };
+
+  config = lib.mkIf config.systemd-boot.enable {
+    boot.loader.timeout = 5;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.consoleMode = "max";
+    boot.loader.systemd-boot.configurationLimit = 14;
+  };
+}
