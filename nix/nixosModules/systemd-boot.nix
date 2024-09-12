95087f64f329033dd9875d04052bc474652ae43e
Daniel Lowry <development@daniellowry.co.uk>
2024-09-13 00:38:39 +0100

    ✨ Added timeout option to systemd-boot nix module

--- nix/nixosModules/systemd-boot.nix
+++ nix/nixosModules/systemd-boot.nix
@@ -1,10 +1,15 @@
 { pkgs, lib, config, ... }: {
   options = {
     systemd-boot.enable = lib.mkEnableOption "enable systemd-boot";
+    systemd-boot.timeout = lib.mkOption {
+      type = lib.types.int;
+      description = "Number of seconds to wait before booting to the first option in the bootloader";
+      default = 0;
+    };
   };
 
   config = lib.mkIf config.systemd-boot.enable {
-    boot.loader.timeout = 5;
+    boot.loader.timeout = config.systemd-boot.timeout;
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.consoleMode = "max";
     boot.loader.systemd-boot.configurationLimit = 14;
