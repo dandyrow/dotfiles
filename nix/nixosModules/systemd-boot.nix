178a72b0ebb578549bbeb1d9cbf09eb266dc0d68
Daniel Lowry <development@daniellowry.co.uk>
2024-09-12 22:00:34 +0100

    🐛 Fixed issue with systemd-boot in Nixos config
    🌐 Added config for locale

--- nix/nixosModules/systemd-boot.nix
+++ nix/nixosModules/systemd-boot.nix
@@ -8,5 +8,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.systemd-boot.consoleMode = "max";
     boot.loader.systemd-boot.configurationLimit = 14;
+
+    boot.loader.efi.canTouchEfiVariables = true;
   };
 }
