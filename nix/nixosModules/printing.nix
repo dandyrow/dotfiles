620eb8b59073fb0b934b03c66540a9cd42307315
Daniel Lowry <development@daniellowry.co.uk>
2024-09-12 22:52:14 +0100

    ✨ Added polkit rule in printing nixos module
    Will allow a user in the print group to configure printers without a
    password.

--- nix/nixosModules/printing.nix
+++ nix/nixosModules/printing.nix
@@ -1,15 +1,30 @@
 { pkgs, lib, config, ... }: {
   options = {
     printing.enable = lib.mkEnableOption "enables printing";
+    passwordless-printer-setup.enable = lib.mkEnableOption "enables polkit for passwordless printer setup for users in the print group";
   };
 
   config = lib.mkIf config.printing.enable {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      cups-pdf.enable = true;
+    };
 
     services.avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
+    };
+
+    security.polkit = lib.mkIf config.passwordless-printer-setup.enable {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (action.id == "org.opensuse.cupspkhelper.mechanism.all-edit" && subject.isInGroup("print")) {
+            return polkit.Result.YES;
+          }
+          });
+      '';
     };
   };
 }
