14a135ff2ff891304977d21cc15a734a33171875
Daniel Lowry <development@daniellowry.co.uk>
2024-09-13 10:58:46 +0100

    ✨ Added boot logo to systemd-boot nixos module

--- nix/nixosModules/systemd-boot.nix
+++ nix/nixosModules/systemd-boot.nix
@@ -6,14 +6,45 @@
       description = "Number of seconds to wait before booting to the first option in the bootloader";
       default = 0;
     };
+    systemd-boot.logo = lib.mkEnableOption "enable boot logo";
+    systemd-boot.theme = lib.mkOption {
+      type = lib.types.str;
+      description = "Boot logo (plymouth) theme";
+      default = "bgrt";
+    };
   };
 
   config = lib.mkIf config.systemd-boot.enable {
-    boot.loader.timeout = config.systemd-boot.timeout;
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.consoleMode = "max";
-    boot.loader.systemd-boot.configurationLimit = 14;
+    boot = {
+      loader = {
+        timeout = config.systemd-boot.timeout;
 
-    boot.loader.efi.canTouchEfiVariables = true;
+        systemd-boot = {
+          enable = true;
+          consoleMode = "max";
+          configurationLimit = 14;
+        };
+
+        efi.canTouchEfiVariables = true;
+      };
+
+      plymouth = lib.mkIf config.systemd-boot.logo {
+        enable = true;
+        theme = config.systemd-boot.theme;
+      };
+
+      # Enable silent boot if logo is enabled
+      consoleLogLevel = lib.mkIf config.systemd-boot.logo 0;
+      initrd.verbose = lib.mkIf config.systemd-boot.logo false;
+      kernelParams = lib.mkIf config.systemd-boot.logo [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+    };
   };
 }
