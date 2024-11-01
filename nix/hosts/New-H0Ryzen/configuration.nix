27e13dc019a56c18be9e0d73a3e1db07b3bd269f
Daniel Lowry <development@daniellowry.co.uk>
2024-11-01 17:06:17 +0000

    ✨ Added New-H0Ryzen nixOS configuration

---
+++ nix/hosts/New-H0Ryzen/configuration.nix
@@ -0,0 +1,51 @@
+{ ... }:
+{
+  systemd-boot.enable = true;
+  # Needs these kernel modules to boot
+  boot.initrd.availableKernelModules = [
+    "uhci_hcd"
+    "ehci_pci"
+    "ahci"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+
+  gnome.enable = true;
+  gnome.enable-gnome-software = true;
+
+  printing.enable = true;
+  passwordless-printer-setup.enable = true;
+
+  networking.hostName = "New-H0Ryzen";
+  services.qemuGuest.enable = true;
+
+  documentation.nixos.enable = false;
+
+  programs.command-not-found.enable = true;
+  programs.firefox.enable = true;
+  programs.git.enable = true;
+
+  users.users = {
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIForH9TNaNvQGNBzWXyPdtRGO5xiR2BYQeIKf8mzN2u9 dandyrow@Desktop"
+      ];
+    };
+
+    dandyrow = {
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "print"
+      ];
+    };
+  };
+
+  system.stateVersion = "24.05";
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+}
