5959262b515c789254db0a22244afc4568cd434f
Daniel Lowry <development@daniellowry.co.uk>
2025-07-11 17:18:01 +0100

    🔧 Changed LSP diagnostic messages in Neovim config

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -5,6 +5,23 @@ vim.lsp.enable({
 vim.diagnostic.config({
 	virtual_lines = { current_line = true },
 	severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		linehl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticError",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+			[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+			[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+		},
+	},
 })
 
 vim.o.winborder = "rounded"
