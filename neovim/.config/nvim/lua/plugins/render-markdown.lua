c88efcb869960708074fabf79b94ceccc0258a2d
Daniel Lowry <development@daniellowry.co.uk>
2025-07-17 17:25:22 +0100

    🚚 Added render-markdown plugin to new Noevim config

---
+++ neovim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+	version = "*",
+	ft = { "markdown" },
+	opts = {
+		completions = { blink = { enabled = true } },
+	},
+}
