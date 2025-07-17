827fb4e0d554977a527c3a6aa884f46985ff48bf
Daniel Lowry <development@daniellowry.co.uk>
2025-07-17 17:22:36 +0100

    💥 Added new Treesitter config to new Neovim config
    Did treesitter in the new rewritten way

---
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").install({ "markdown", "markdown-inline" })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "markdown" },
+			callback = function()
+				vim.treesitter.start()
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
+}
