ca607e609b10ce4228f5ee196605be92ab4649f2
Daniel Lowry <development@daniellowry.co.uk>
2025-07-18 13:02:02 +0100

    ✨ Added conform code formatting plugin to new Neovim config
    This replaces nonels from my old config

---
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    default_format_opts = { lsp_format = "fallback" },
+    format_on_save = { timeout_ms = 500 },
+  },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true })
+      end,
+      desc = "Format current file",
+    },
+  },
+}
