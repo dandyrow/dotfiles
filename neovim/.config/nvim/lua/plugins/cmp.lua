c45ab1b491701ab57bbc2f5868e9ee48b350f6e0
Daniel Lowry <development@daniellowry.co.uk>
2025-07-18 18:35:06 +0100

    ✨ Added LazyDev to new Neovim config
    Also added stylua file for correct formatting

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -1,81 +1,86 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"moyiz/blink-emoji.nvim",
-	},
-	version = "1.*",
-	opts = {
-		keymap = {
-			preset = "super-tab",
-			["<CR>"] = { "accept", "fallback" },
-		},
+  "saghen/blink.cmp",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "moyiz/blink-emoji.nvim",
+  },
+  version = "1.*",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<CR>"] = { "accept", "fallback" },
+    },
 
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
 
-		completion = {
-			documentation = { auto_show = true, auto_show_delay_ms = 500 },
-			ghost_text = { enabled = true },
-			list = { selection = { preselect = false, auto_insert = false } },
-			menu = {
-				draw = {
-					columns = {
-						{
-							"kind_icon",
-							"label",
-							gap = 1,
-							"kind",
-						},
-					},
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      ghost_text = { enabled = true },
+      list = { selection = { preselect = false, auto_insert = false } },
+      menu = {
+        draw = {
+          columns = {
+            {
+              "kind_icon",
+              "label",
+              gap = 1,
+              "kind",
+            },
+          },
 
-					components = {
-						kind_icon = {
-							-- (optional) use highlights from mini.icons
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
-						kind = {
-							-- (optional) use highlights from mini.icons
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
-					},
+          components = {
+            kind_icon = {
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
 
-					treesitter = { "lsp", "snippets" },
-				},
-			},
-		},
+          treesitter = { "lsp", "snippets" },
+        },
+      },
+    },
 
-		signature = { enabled = true },
+    signature = { enabled = true },
 
-		cmdline = {
-			enabled = true,
-			completion = { menu = { auto_show = true } },
-			keymap = { ["<Tab>"] = { "show", "accept" } },
-		},
+    cmdline = {
+      enabled = true,
+      completion = { menu = { auto_show = true } },
+      keymap = { ["<Tab>"] = { "show", "accept" } },
+    },
 
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+    sources = {
+      default = { "lazydev", "lsp", "path", "snippets", "buffer", "emoji" },
 
-			providers = {
-				emoji = {
-					module = "blink-emoji",
-					name = "Emoji",
-					score_offset = 15, -- Tune by preference
-					opts = { insert = true }, -- Insert emoji (default) or complete its name
-				},
-			},
-		},
+      providers = {
+        lazydev = {
+          name = "LzayDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+        emoji = {
+          module = "blink-emoji",
+          name = "Emoji",
+          score_offset = 15, -- Tune by preference
+          opts = { insert = true }, -- Insert emoji (default) or complete its name
+        },
+      },
+    },
 
-    fuzzy = { implementation = "lua" },  -- Rust binary won't download due to 403 error
-	},
+    fuzzy = { implementation = "lua" }, -- Rust binary won't download due to 403 error
+  },
 
-	opts_extend = { "sources.default" },
+  opts_extend = { "sources.default" },
 }
