532287be6b106e2d6a349a6ad8e050beccb7db81
Daniel Lowry <development@daniellowry.co.uk>
2025-07-18 20:11:12 +0100

    🐛 Fixed issue with gitsigns Neovim plugin
    Issue was lazy loading was causing the signs not to appear. This fixes
    that by using on_attach to create key bindings rather than Lazy's keys
    property

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,95 +1,48 @@
 return {
   "lewis6991/gitsigns.nvim",
-
   opts = {
     current_line_blame = true,
     attach_to_untracked = true,
-  },
+    on_attach = function(bufnr)
+      local gitsigns = require("gitsigns")
 
-  keys = {
-    {
-      "]h",
-      "<CMD>Gitsigns next_hunk<CR>",
-      desc = "Git hunk next",
-    },
-    {
-      "[h",
-      "<CMD>Gitsigns prev_hunk<CR>",
-      desc = "Git hunk previous",
-    },
-    {
-      "<leader>hs",
-      "<CMD>Gitsigns stage_hunk<CR>",
-      desc = "Git hunk stage",
-    },
-    {
-      "<leader>hr",
-      "<CMD>Gitsigns reset_hunk<CR>",
-      desc = "Git hunk reset",
-    },
-    {
-      "<leader>hs",
-      function()
-        require("gitsigns").stage_hunk({
-          vim.fn.line("."),
-          vim.fn.line("v"),
-        })
-      end,
-      mode = "v",
-      desc = "Git hunk stage",
-    },
-    {
-      "<leader>hr",
-      function()
-        require("gitsigns").reset_hunk({
-          vim.fn.line("."),
-          vim.fn.line("v"),
-        })
-      end,
-      mode = "v",
-      desc = "Git hunk reset",
-    },
-    {
-      "<leader>hS",
-      "<CMD>Gitsigns stage_buffer<CR>",
-      desc = "Git buffer stage"
-    },
-    {
-      "<leader>hR",
-      "<CMD>Gitsigns reset_buffer<CR>",
-      desc = "Git buffer reset",
-    },
-    {
-      "<leader>hp",
-      "<CMD>Gitsigns preview_hunk<CR>",
-      desc = "Git hunk preview",
-    },
-    {
-      "<leader>hi",
-      "<CMD>Gitsigns preview_hunk_inline<CR>",
-      desc = "Git hunk preview (inline)"
-    },
-    {
-      "<leader>hb",
-      function()
-        require("gitsigns").blame_line({ full = true })
-      end,
-      desc = "Git hunk blame line",
-    },
-    {
-      "<leader>hu",
-      "<CMD>Gitsigns undo_stage_hunk<CR>",
-      desc = "Git hunk unstage",
-    },
-    {
-      "<leader>hv",
-      "<CMD>Gitsigns select_hunk<CR>",
-      desc = "Git hunk select",
-    },
-    {
-      "<leader>hd",
-      "<CMD>Gitsigns toggle_deleted<CR>",
-      desc = "Git hunk toggle deleted",
-    },
+      local function map(lhs, rhs, desc, mode)
+        local opts = { desc = desc, buffer = bufnr }
+        mode = mode or "n"
+        vim.keymap.set(mode, lhs, rhs, opts)
+      end
+
+      -- Navigation
+      map("]h", function()
+        gitsigns.nav_hunk({ "next" })
+      end, "Navigate to next hunk")
+
+      map("[h", function()
+        gitsigns.nav_hunk({ "prev" })
+      end, "Navigate to previous hunk")
+
+      -- Actions
+      map("<leader>hs", gitsigns.stage_hunk, "Stage hunk")
+      map("<leader>hs", function()
+        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "Stage hunk", "v")
+
+      map("<leader>hr", gitsigns.reset_hunk, "Reset hunk")
+      map("<leader>hr", function()
+        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "Reset hunk")
+
+      map("<leader>hS", gitsigns.stage_buffer, "Stage buffer")
+      map("<leader>hR", gitsigns.reset_buffer, "Reset buffer")
+      map("<leader>hp", gitsigns.preview_hunk, "Preview hunk")
+      map("<leader>hq", gitsigns.setqflist, "Show quickfix list")
+      map("<leader>hw", gitsigns.toggle_word_diff, "Toggle word diff")
+
+      map("<leader>hb", function()
+        gitsigns.blame_line({ full = true })
+      end, "Hunk blame")
+
+      map("ih", gitsigns.select_hunk, "Select hunk", { "o", "x" })
+    end,
   },
 }
