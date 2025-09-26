eb71216ca143e8a50e6e9fc068cb53b8eb2a9e7a
Daniel Lowry <development@daniellowry.co.uk>
2025-09-26 11:31:24 +0100

    💄 Added macro recording message to lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,17 +1,29 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	opts = {
-		options = {
-			theme = "catppuccin",
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "filename", "filesize" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "searchcount", "selectioncount", "location" },
-			lualine_z = { "tabs" },
-		},
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename", "filesize" },
+        lualine_x = {
+          {
+            ---@diagnostic disable: deprecated, undefined-field
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            ---@diagnostic enable: deprecated, undefined-field
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "searchcount", "selectioncount", "location" },
+        lualine_z = { "tabs" },
+      },
+    })
+  end,
 }
