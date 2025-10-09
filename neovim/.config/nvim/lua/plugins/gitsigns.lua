6c91af298b0fafaa4769374ccb42a259db492f1e
Daniel Lowry <development@daniellowry.co.uk>
2025-10-09 11:35:24 +0100

    🔧 Updated gitsigns keymaps in Neovim config

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -13,13 +13,21 @@ return {
       end
 
       -- Navigation
-      map("]h", function()
-        gitsigns.nav_hunk({ "next" })
-      end, "Navigate to next hunk")
+      map("]c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gitsigns.nav_hunk("next")
+        end
+      end, "Navigate to next change")
 
-      map("[h", function()
-        gitsigns.nav_hunk({ "prev" })
-      end, "Navigate to previous hunk")
+      map("[c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gitsigns.nav_hunk("prev")
+        end
+      end, "Navigate to previous change")
 
       -- Actions
       map("<leader>hs", gitsigns.stage_hunk, "Stage hunk")
@@ -30,17 +38,24 @@ return {
       map("<leader>hr", gitsigns.reset_hunk, "Reset hunk")
       map("<leader>hr", function()
         gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end, "Reset hunk")
+      end, "Reset hunk", "v")
 
       map("<leader>hS", gitsigns.stage_buffer, "Stage buffer")
       map("<leader>hR", gitsigns.reset_buffer, "Reset buffer")
       map("<leader>hp", gitsigns.preview_hunk, "Preview hunk")
-      map("<leader>hq", gitsigns.setqflist, "Show quickfix list")
-      map("<leader>hw", gitsigns.toggle_word_diff, "Toggle word diff")
+      map("<leader>hi", gitsigns.preview_hunk_inline, "Preview hunk inline")
 
       map("<leader>hb", function()
         gitsigns.blame_line({ full = true })
       end, "Hunk blame")
+
+      map("<leader>hq", gitsigns.setqflist, "Show quickfix list for buffer")
+      map("<leader>hQ", function()
+        gitsigns.setqflist("all")
+      end, "Show quickfix list for project")
+
+      map("<leader>hb", gitsigns.toggle_current_line_blame, "Toggle current line blame")
+      map("<leader>hw", gitsigns.toggle_word_diff, "Toggle word diff")
 
       map("ih", gitsigns.select_hunk, "Select hunk", { "o", "x" })
     end,
