46fe44cc829818beeb03db06366d0c31523e3dcb
Daniel Lowry <development@daniellowry.co.uk>
2026-03-19 00:07:19 +0000

    ⬆️ Update Neovim dependencies

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "catppuccin",
+        theme = "catppuccin-nvim",
       },
       sections = {
         lualine_a = { "mode" },
