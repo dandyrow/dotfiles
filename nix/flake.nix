80d407cfdf9c300afab9b5f00d8236a1b5be95ac
Daniel Lowry <development@daniellowry.co.uk>
2026-05-19 15:29:49 +0100

    🔥 Removed LivsLaptop config
    This is not being used anymore and I don't plan on using flatpaks on my
    systems so wanting to remove this dependency.

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/v0.4.1";
 
     disko = {
       url = "github:nix-community/disko";
@@ -26,13 +25,6 @@
     }@inputs:
     {
       nixosConfigurations = {
-        LivsLaptop = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./hosts/LivsLaptop/configuration.nix
-            ./nixosModules
-            nix-flatpak.nixosModules.nix-flatpak
-          ];
-        };
 
         New-H0Ryzen = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
