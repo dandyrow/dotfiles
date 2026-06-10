73a81f30e2133545f1976732d37683661b4d81cb
Daniel Lowry <development@daniellowry.co.uk>
2026-06-10 18:13:09 +0100

    📦 feat(nix/desktop): Added photo thumbnails for raw files

--- nix/modules/profiles/desktop.nix
+++ nix/modules/profiles/desktop.nix
@@ -15,5 +15,23 @@
     vlc
     darktable
     libreoffice-fresh
+
+    # Video Thumbnailer
+    ffmpeg-headless
+    ffmpegthumbnailer
+
+    # Raw Thumbnailer
+    gdk-pixbuf
+    # Allow gdk-pixbuf to thumbnail RAW photos by extracting the embedded jpeg
+    (writeTextFile {
+      name = "raw-embedded-jpeg-thumbnailer";
+      destination = "/share/thumbnailers/raw-embedded-jpeg.thumbnailer";
+      text = ''
+        [Thumbnailer Entry]
+        TryExec=gdk-pixbuf-thumbnailer
+        Exec=gdk-pixbuf-thumbnailer -s %s %u %o
+        MimeType=image/x-canon-crw;image/x-canon-cr2;image/x-canon-cr3;image/x-adobe-dng;image/x-dng;
+      '';
+    })
   ];
 }
